57744e2292eb7a96a685edffe10db423b01f580b
MortalMiro <mintaoh@student.unimelb.edu.au>
2023-02-07 16:13:56 +0800

    show selection of image in asset and return coordinates

--- test.py
+++ test.py
@@ -84,12 +84,10 @@ def visualize_test(asset_path, img_path, device, matching):
             else:
                 if matches[valid].shape[0] >= max(4, int(math.floor(kpts1.shape[0]) / 3)):
                     make_matching_plot(image0[j], image1[i], kpts0, kpts1, mkpts0, mkpts1, color, True)
-                    
-                    
-# Find singe match
-def find_mpts(image0, image1, matching, device, num_mkpts):
-    max_pair = None
 
+
+def find_mpts(image0, image1, matching, device, num_kpts, num_mkpts):
+    max_pair = None
     for i in range(len(image1)):
         for j in range(len(image0)):
             torch.cuda.empty_cache()
@@ -110,46 +108,48 @@ def find_mpts(image0, image1, matching, device, num_mkpts):
                 if matches[valid].shape[0] >= max(4, int(math.floor(kpts1.shape[0]) * 0.7)):
                     if matches[valid].shape[0] > num_mkpts:
                         num_mkpts = matches[valid].shape[0]
-                        max_pair = (image0[j], image1[i], kpts0, kpts1, mkpts0, mkpts1)
+                        max_pair = (kpts0, kpts1, mkpts0, mkpts1, j)
             else:
                 if matches[valid].shape[0] >= max(4, int(math.floor(kpts1.shape[0]) / 3)):
                     if matches[valid].shape[0] > num_mkpts:
                         num_mkpts = matches[valid].shape[0]
-                        max_pair = (image0[j], image1[i], kpts0, kpts1, mkpts0, mkpts1)
+                        max_pair = (kpts0, kpts1, mkpts0, mkpts1, j)
 
     if num_mkpts == 0:
         return None
     else:
         return max_pair
-        print("BINGO!")
-        make_matching_plot(max_pair[0], max_pair[1], max_pair[2], max_pair[3], max_pair[4], max_pair[5], color, True)
 
 
-def find_match(asset_path, img_path, device, matching):
+def find_match(show_selection, asset_path, img_path, device, matching):
     color = 'r'
+    num_kpts = 0
     num_mkpts = 0
-    image0, image1 = read_image_test(asset_path, img_path, False)
-    max_pair = find_mpts(image0, image1, matching, device, num_mkpts)
+    image0, image1, l_image0_pos, scale0, asset, img = read_image_test(asset_path, img_path, False)
+    max_pair = find_mpts(image0, image1, matching, device, num_kpts, num_mkpts)
+
     if max_pair is not None:
         print("BINGO!")
-        make_matching_plot(max_pair[0], max_pair[1], max_pair[2], max_pair[3], max_pair[4], max_pair[5], color, True)
+        max_pair = (*max_pair, asset, img)
+        make_matching_plot(max_pair, color, False, show_selection, l_image0_pos, scale0)
         return 0
 
-    image0, image1 = read_image_test(asset_path, img_path, True)
-    max_pair = find_mpts(image0, image1, matching, device, num_mkpts)
+    image0, image1, l_image0_pos, scale0, asset, img = read_image_test(asset_path, img_path, True)
+    max_pair = find_mpts(image0, image1, matching, device, num_kpts, num_mkpts)
     if max_pair is not None:
         print("BINGO!")
-        make_matching_plot(max_pair[0], max_pair[1], max_pair[2], max_pair[3], max_pair[4], max_pair[5], color, True)
+        max_pair = (*max_pair, asset, img)
+        make_matching_plot(max_pair, color, False, show_selection, l_image0_pos, scale0)
         return 0
     else:
         print("NOT FOUND")
         return 0
 
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PICMATCHING')
     parser.add_argument('-asset', default=None)
     parser.add_argument('-img', default=None)
+    parser.add_argument('-show_selection', default=True)
     args = parser.parse_args()
 
     # config for superpoint and superglue
@@ -170,97 +170,79 @@ if __name__ == "__main__":
     torch.set_grad_enabled(False)
     matching = Matching(config).eval().to(device)
 
-    find_match(args.asset, args.img, device, matching)
+    find_match(args.show_selection,args.asset, args.img, device, matching)
 
 
-# # config for superpoint and superglue
-# config = {
-#         'superpoint': {
-#             'nms_radius':  2,
-#             'keypoint_threshold': 0.005,
-#             'max_keypoints': 15000
-#         },
-#         'superglue': {
-#             'weights': 'picmatch',
-#             'sinkhorn_iterations': 100,
-#             'match_threshold': 0.4,
-#         }
-#     }
 
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# torch.set_grad_enabled(False)
-# matching = Matching(config).eval().to(device)
+# Test single instance
+# asset_path = 'data/test/img/normal_pack_199.png'
+# img_path = 'data/test/temp/26332.png'
+# prediction = visualize_test(asset_path, img_path, device,matching)
 
-
-# # Test single instance
-# # asset_path = 'data/test/img/normal_pack_199.png'
-# # img_path = 'data/test/temp/26332.png'
-# # prediction = visualize_test(asset_path, img_path, device,matching)
-
-# # print('finish')
+# print('finish')
 
 # # Read annotation file
 # data = pd.read_csv("data/test/labels.csv")
-
+#
 # # shuffle pos data
 # pos_data= data.iloc[273908:273908+7500,:]
 # pos_data = pos_data.sample(frac=1)
-
+#
 # # shuffle neg data
 # neg_data = data.iloc[:273908,:]
 # neg_data = neg_data.sample(frac=1)
 # neg_data = neg_data.iloc[:7500,:]
-
+#
 # img_root = "data/test/temp/"
 # asset_root = "data/test/img/"
-
-
+#
+#
 # # Recall = (TP/TP+FN)
 # num_correct_predictions = 0
 # l_batch_no = []
 # l_acc = []
-
+#
 # for i in range(len(pos_data)):
 #     asset_path = os.path.join(asset_root, pos_data.iloc[i, 0])
 #     img_path = os.path.join(img_root, pos_data.iloc[i, 1])
 #     prediction = test(asset_path, img_path, device,matching)
-
+#
 #     if prediction == int(pos_data.iloc[i, 2]):
 #         num_correct_predictions += 1
 #     print('pos_idx:', i+1)
 #     print('acc:', num_correct_predictions / (i + 1))
-
+#
 #     if (i+1) % 500 == 0:
 #         batch_no = int((i+1) / 500)
 #         l_batch_no.append(batch_no)
 #         print("number of predictions: ", i+1)
-
+#
 #         accuracy = num_correct_predictions / (i+1)
 #         l_acc.append(accuracy)
 #         print("current accuracy is: ", accuracy)
-
+#
 # # plot the figure
 # plt.figure()
 # plt.plot(l_batch_no, l_acc)
 # plt.xticks(range(1, max(l_batch_no)+1))
 # plt.savefig('recall_result.png')
-
-
+#
+#
 # # Specificity = TN/TN+FP
 # num_correct_predictions = 0
 # l_batch_no = []
 # l_acc = []
-
+#
 # for i in range(len(neg_data)):
 #     asset_path = os.path.join(asset_root, neg_data.iloc[i, 0])
 #     img_path = os.path.join(img_root, neg_data.iloc[i, 1])
 #     prediction = test(asset_path, img_path, device,matching)
-
+#
 #     if prediction == int(neg_data.iloc[i, 2]):
 #             num_correct_predictions += 1
 #     print('neg_idx:', i+1)
 #     print('acc:', num_correct_predictions/(i+1))
-
+#
 #     if (i+1) % 500 == 0:
 #         batch_no = int((i+1)/500)
 #         l_batch_no.append(batch_no)
@@ -268,7 +250,7 @@ if __name__ == "__main__":
 #         accuracy = num_correct_predictions/(i+1)
 #         l_acc.append(accuracy)
 #         print("current accuracy is: ", num_correct_predictions/(i+1))
-        
+#
 # # plot the figure
 # plt.figure()
 # plt.plot(l_batch_no, l_acc)
--- utils.py
+++ utils.py
@@ -3,10 +3,11 @@ import torch
 import matplotlib.pyplot as plt
 import matplotlib
 import math
+import numpy as np
 
+# [None, None] == .unsqueeze(0).unsqueeze(0)
 def frame2tensor(frame, device):
     return torch.from_numpy(frame/255.).float()[None, None].to(device)
-
 
 def read_image_test(path0, path1, segmentation):
     torch.cuda.empty_cache()
@@ -14,11 +15,15 @@ def read_image_test(path0, path1, segmentation):
     image1 = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
 
     if image0 is None or image1 is None:
-        return None, None, None, None
+        return None, None, None, None, None, None
+
+    asset = image0
+    img = image1
 
     h1, w1 = image1.shape[:2]
     h0, w0 = image0.shape[:2]
     l_image0 = []
+    l_image0_pos = []
     num_vertical_windows = 1
     num_horizontal_windows = 1
 
@@ -38,14 +43,17 @@ def read_image_test(path0, path1, segmentation):
                 w_end = min(w_start+segment_factor*w1, w0)
                 if h_start != h0 and w_start != w0:
                     l_image0.append(image0[h_start:h_end, w_start:w_end])
+                    l_image0_pos.append((h_start, w_start))
     else:
         l_image0.append(image0)
+        l_image0_pos.append((0, 0))
 
 
     if min(h1, w1) <= 64:
        scale0 = 128 / min(h1, w1)
     else:
        scale0 = 1
+
     l_resized_image0 = []
     # scale_factor = max(num_horizontal_windows, num_vertical_windows)
     if scale0 != 1:
@@ -86,7 +94,7 @@ def read_image_test(path0, path1, segmentation):
     # for resized_crop in l_resized_image0:
     #     l_inp0.append(frame2tensor(resized_crop, device))
 
-    return l_resized_image0, l_image1
+    return l_resized_image0, l_image1, l_image0_pos, scale0, asset, img
 
 def plot_image_pair(imgs, dpi=100, size=6, pad=.5):
     n = len(imgs)
@@ -108,29 +116,80 @@ def plot_keypoints(kpts0, kpts1, color='w', ps=2):
     ax[1].scatter(kpts1[:, 0], kpts1[:, 1], c=color, s=ps)
 
 
-def plot_matches(kpts0, kpts1, color, lw=1.5, ps=4):
+def plot_matches(asset_idx, l_image0_pos, scale0, show_selection, kpts0, kpts1, color, lw=1.5, ps=4):
     fig = plt.gcf()
     ax = fig.axes
     fig.canvas.draw()
 
     transFigure = fig.transFigure.inverted()
-    fkpts0 = transFigure.transform(ax[0].transData.transform(kpts0))
-    fkpts1 = transFigure.transform(ax[1].transData.transform(kpts1))
 
-    fig.lines = [matplotlib.lines.Line2D(
-        (fkpts0[i, 0], fkpts1[i, 0]), (fkpts0[i, 1], fkpts1[i, 1]), zorder=1,
-        transform=fig.transFigure, c='r', linewidth=lw)
-                 for i in range(len(kpts0))]
-    ax[0].scatter(kpts0[:, 0], kpts0[:, 1], c=color, s=ps)
-    ax[1].scatter(kpts1[:, 0], kpts1[:, 1], c=color, s=ps)
+    # Delete outliers by Z-score
+    mean_p = np.mean(kpts0, axis=0)
+
+    man_dist = []
+    for point in kpts0:
+        man_dist.append(math.sqrt(math.pow(point[0]-mean_p[0],2) + math.pow(point[1]-mean_p[1],2)))
+    mean_dist = np.mean(man_dist)
+    std_dist = np.std(man_dist)
+
+    outlier = []
+    threshold = 3
+    for i in range(len(man_dist)):
+        z = (man_dist[i] - mean_dist) / std_dist
+        if z > threshold:
+            outlier.append(i)
+
+    # Discard outliers
+    l_kpts0 = kpts0.tolist()
+    for idx in outlier:
+        del l_kpts0[idx]
+    kpts0 = np.array(l_kpts0)
+
+    # Revert coordinates
+    l_ori_kpts0 = []
+    for point in kpts0:
+        l_ori_kpts0.append([l_image0_pos[asset_idx][1] + point[0]/scale0, l_image0_pos[asset_idx][0] + point[1]/scale0])
+    ori_kpts0 = np.array(l_ori_kpts0)
+
+    # Select image in asset
+    min_p = np.amin(ori_kpts0, axis=0)
+    max_p = np.amax(ori_kpts0, axis=0)
+    print("Suggested top left coordiantes = ", (min_p[0], min_p[1]))
+    print("Suggested bottom right coordiantes = ", (max_p[0], max_p[1]))
+
+    # Show selection
+    if show_selection:
+        fkpts0 = transFigure.transform(ax[0].transData.transform(ori_kpts0))
+        fmin_p  = np.amin(fkpts0, axis=0)
+        fmax_p = np.amax(fkpts0, axis=0)
+
+        lines = []
+        lines.append(matplotlib.lines.Line2D((fmin_p[0], fmax_p[0]), (fmin_p[1], fmin_p[1]), zorder=1, transform=fig.transFigure, c='r', linewidth=lw))
+        lines.append(
+            matplotlib.lines.Line2D((fmin_p[0], fmax_p[0]), (fmax_p[1], fmax_p[1]), zorder=1, transform=fig.transFigure,
+                                    c='r', linewidth=lw))
+        lines.append(
+            matplotlib.lines.Line2D((fmin_p[0], fmin_p[0]), (fmin_p[1], fmax_p[1]), zorder=1, transform=fig.transFigure,
+                                    c='r', linewidth=lw))
+        lines.append(
+            matplotlib.lines.Line2D((fmax_p[0], fmax_p[0]), (fmin_p[1], fmax_p[1]), zorder=1, transform=fig.transFigure,
+                                    c='r', linewidth=lw))
+        fig.lines = lines
 
 
-def make_matching_plot(image0, image1, kpts0, kpts1, mkpts0, mkpts1, color, show_keypoints):
+    # Show center
+    # mean_p = np.mean(kpts0, axis=0)
+    # ori_mean_p_x = l_image0_pos[asset_idx][1] + mean_p[0]/scale0
+    # ori_mean_p_y = l_image0_pos[asset_idx][0] + mean_p[1]/scale0
+    # ax[0].scatter(ori_mean_p_x, ori_mean_p_y, c='r', s=ps)
 
-    plot_image_pair([image0, image1])
+
+def make_matching_plot(max_pair, color, show_keypoints, show_selection, l_image0_pos, scale0):
+    kpts0, kpts1, mkpts0, mkpts1, asset_idx, asset, img = max_pair
+    plot_image_pair([asset, img])
     if show_keypoints:
         plot_keypoints(kpts0, kpts1, color='w', ps=4)
         plot_keypoints(kpts0, kpts1, color='w', ps=2)
-    plot_matches(mkpts0, mkpts1, color)
+    plot_matches(asset_idx, l_image0_pos, scale0, show_selection, mkpts0, mkpts1, color)
 
     plt.show()
